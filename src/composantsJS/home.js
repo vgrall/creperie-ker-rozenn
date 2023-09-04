3ac19c7983434c9c75bf62ae7286a4143d9ceefa
virginie <virginie.grall@gmail.com>
2023-09-04 09:31:19 +0200

    effet typewriter sur titre

--- src/composantsJS/home.js
+++ src/composantsJS/home.js
@@ -1,4 +1,62 @@
 /* *****************************************************************
+ANIMATION TITRE KER ROZENN
+***************************************************************** */
+
+// Déclare une variable appelée "customCursor" et la relie à un élément HTML ayant la classe "custom-cursor".
+const customCursor = document.querySelector(".custom-cursor");
+
+// Ajoute un écouteur d'événements pour suivre le mouvement de la souris dans la fenêtre du navigateur.
+window.addEventListener("mousemove", handleCustomCursor);
+
+// Crée une fonction appelée "handleCustomCursor" qui s'exécute à chaque mouvement de la souris.
+function handleCustomCursor(e) {
+  // Déplace le curseur personnalisé pour suivre la position de la souris.
+  customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
+}
+
+// Déclare plusieurs variables et les relie à des éléments HTML spécifiques.
+const title = document.querySelector("h1");
+const subtitle = document.querySelector(".subtitle");
+const heroPushLink = document.querySelector(".hero-push-link");
+const txt = "KER ROZENN";
+
+// Crée une fonction appelée "typewriter" pour simuler l'effet d'une machine à écrire.
+function typewriter(text, index) {
+  // Si l'index est supérieur à 3, ajoute la classe "active" à l'élément "subtitle".
+  if (index > 3) subtitle.classList.add("active");
+  // Si l'index est supérieur à 6, ajoute la classe "active" à l'élément "heroPushLink".
+  if (index > 6) heroPushLink.classList.add("active");
+  // Si l'index est inférieur à la longueur du texte, continue à ajouter des caractères un par un.
+  if (index < text.length) {
+    setTimeout(() => {
+      // Ajoute un caractère du texte au titre (entouré de balises HTML <span>).
+      title.innerHTML += `<span>${text[index]}</span>`;
+      // Appelle la fonction "typewriter" de manière récursive pour le caractère suivant.
+      typewriter(text, index + 1);
+    }, 150); // Attends 150 millisecondes avant d'ajouter le caractère suivant.
+  }
+}
+
+// Après 300 millisecondes, commence à appeler la fonction "typewriter" avec le texte "KER ROZENN".
+setTimeout(() => {
+  typewriter(txt, 0);
+}, 300);
+
+// Ajoute un écouteur d'événements pour gérer le clic sur l'élément "heroPushLink".
+heroPushLink.addEventListener("click", slideDown);
+
+// Crée une fonction appelée "slideDown" pour faire défiler la page vers le bas.
+function slideDown(e) {
+  // Empêche le comportement par défaut du lien (ne pas suivre le lien).
+  e.preventDefault();
+  // Fait défiler la page vers le haut de l'élément cible spécifié dans le lien.
+  window.scrollTo({
+    top: document.querySelector(`${e.target.getAttribute("href")}`).offsetTop,
+    behavior: "smooth", // Fait défiler en douceur plutôt que brusquement.
+  });
+}
+
+/* *****************************************************************
 EFFET DE SCROLL et POPUP (menu carte et concours)
 ***************************************************************** */
 let playOnce = true;
@@ -12,7 +70,7 @@ window.addEventListener("scroll", () => {
   let scrollValue =
     (window.innerHeight + window.scrollY) / document.body.offsetHeight;
 
-  if (scrollValue > 0.65) {
+  if (scrollValue > 0.5) {
     containerCarte.style.opacity = 1; //rend visible l'élément
     containerCarte.style.transform = "none"; //
   }
@@ -55,3 +113,5 @@ function obtenirValeurEmail() {
   // Affichez la valeur dans la console (ou utilisez-la comme vous le souhaitez)
   console.log("Adresse e-mail : " + valeurEmail);
 }
+
+/* ***************************************************************** */
