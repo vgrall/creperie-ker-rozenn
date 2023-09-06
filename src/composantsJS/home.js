6a5a8f616fed266b8e256a4604a811ab80bf5631
virginie <virginie.grall@gmail.com>
2023-09-06 15:04:12 +0200

    correction typewriter js

--- src/composantsJS/home.js
+++ src/composantsJS/home.js
@@ -22,46 +22,29 @@ function closeNav() {
 /* *****************************************************************
 ANIMATION TITRE KER ROZENN
 ***************************************************************** */
+document.addEventListener("DOMContentLoaded", function () {
+  // Déclare plusieurs variables et les relie à des éléments HTML spécifiques.
+  const title = document.querySelector("h1");
+  const txt = "KER ROZENN";
 
-// Déclare une variable appelée "customCursor" et la relie à un élément HTML ayant la classe "custom-cursor".
-const customCursor = document.querySelector(".custom-cursor");
-
-// Ajoute un écouteur d'événements pour suivre le mouvement de la souris dans la fenêtre du navigateur.
-window.addEventListener("mousemove", handleCustomCursor);
-
-// Crée une fonction appelée "handleCustomCursor" qui s'exécute à chaque mouvement de la souris.
-function handleCustomCursor(e) {
-  // Déplace le curseur personnalisé pour suivre la position de la souris.
-  customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
-}
-
-// Déclare plusieurs variables et les relie à des éléments HTML spécifiques.
-const title = document.querySelector("h1");
-const subtitle = document.querySelector(".subtitle");
-const heroPushLink = document.querySelector(".hero-push-link");
-const txt = "KER ROZENN";
-
-// Crée une fonction appelée "typewriter" pour simuler l'effet d'une machine à écrire.
-function typewriter(text, index) {
-  // Si l'index est supérieur à 3, ajoute la classe "active" à l'élément "subtitle".
-  if (index > 3) subtitle.classList.add("active");
-  // Si l'index est supérieur à 6, ajoute la classe "active" à l'élément "heroPushLink".
-  if (index > 6) heroPushLink.classList.add("active");
-  // Si l'index est inférieur à la longueur du texte, continue à ajouter des caractères un par un.
-  if (index < text.length) {
-    setTimeout(() => {
-      // Ajoute un caractère du texte au titre (entouré de balises HTML <span>).
-      title.innerHTML += `<span>${text[index]}</span>`;
-      // Appelle la fonction "typewriter" de manière récursive pour le caractère suivant.
-      typewriter(text, index + 1);
-    }, 150); // Attends 150 millisecondes avant d'ajouter le caractère suivant.
+  // Crée une fonction appelée "typewriter" pour simuler l'effet d'une machine à écrire.
+  function typewriter(text, index) {
+    // Si l'index est inférieur à la longueur du texte, continue à ajouter des caractères un par un.
+    if (index < text.length) {
+      setTimeout(() => {
+        // Ajoute un caractère du texte au titre (entouré de balises HTML <span>).
+        title.innerHTML += `<span>${text[index]}</span>`;
+        // Appelle la fonction "typewriter" de manière récursive pour le caractère suivant.
+        typewriter(text, index + 1);
+      }, 150); // Attends 150 millisecondes avant d'ajouter le caractère suivant.
+    }
   }
-}
 
-// Après 300 millisecondes, commence à appeler la fonction "typewriter" avec le texte "KER ROZENN".
-setTimeout(() => {
-  typewriter(txt, 0);
-}, 300);
+  // commence à appeler la fonction "typewriter" avec le texte "KER ROZENN".
+  setTimeout(() => {
+    typewriter(txt, 0);
+  }, 0);
+});
 
 /* *****************************************************************
 EFFET DE SCROLL et POPUP (menu carte et concours)
@@ -76,14 +59,14 @@ window.addEventListener("scroll", () => {
 
   let scrollValue =
     (window.innerHeight + window.scrollY) / document.body.offsetHeight;
-
+  console.log(scrollValue);
   if (scrollValue > 0.5) {
     containerCarte.style.opacity = 1; //rend visible l'élément
     containerCarte.style.transform = "none"; //
   }
   // Popup
 
-  if (scrollValue > 1 && playOnce) {
+  if (scrollValue > 0.4 && playOnce) {
     popup.style.opacity = 1;
     popup.style.transform = "none";
     playOnce = false;
